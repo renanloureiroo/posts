2d6e3c3b9f3c5d93a3144057b23c495f34a0b6ad
Renan Loureiro <renanpl47@gmail.com>
2021-04-21 18:37:07 -0300

    componetização feita

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import "./App.css";
 import { Component } from "react";
 
+import "./App.css";
+
+import { Posts } from "./components/Posts";
+import { loadPosts } from "./ultis/load-posts";
 class App extends Component {
   state = {
     posts: [],
@@ -8,42 +11,21 @@ class App extends Component {
   timeoutUpdate = null;
 
   // Componente montado
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url };
-    });
-
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   };
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map((post) => (
-            <div className="post">
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+//Usando destructuring já np lugar de props
+export const PostCard = ({ post }) => {
+  return (
+    <div className="post">
+      <img src={post.cover} alt={post.title} />
+      <div className="post-content">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+    </div>
+  );
+};
