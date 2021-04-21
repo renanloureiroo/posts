5c38e5a9fc66d1cc1b31f52023f7ebd661252499
Renan Loureiro <renanpl47@gmail.com>
2021-04-21 12:02:11 -0300

    Terminada aula 17, data fetch

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import "./App.css"
-import { Component } from "react"
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: "O título 1",
-        body: "O corpo 1"
-      },
-      {
-        id: 2,
-        title: "O título 2",
-        body: "O corpo 2"
-      },
-      {
-        id: 3,
-        title: "O título 3",
-        body: "O corpo 3"
-      },
-    ],
+    posts: [],
+  };
+  timeoutUpdate = null;
+
+  // Componente montado
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: postsAndPhotos });
   };
 
   render() {
-    const { posts } = this.state
+    const { posts, counter } = this.state;
 
     return (
-      <div className="App">
-        {posts.map((post) => (
-          <div key={post.id} className="div">
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
-    )
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
   }
 }
 
-export default App
+export default App;
